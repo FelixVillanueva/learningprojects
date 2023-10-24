86e0836e78680cb8d1075e39902254d6795dde63
Félix Jesús Villanueva <felix.villanueva@uclm.es>
2023-10-24 10:46:03 +0200

    step 4

--- angular-node/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-node/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 
 
 @Component({
@@ -9,11 +11,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
   selectedHero?: Hero;
 
+  heroes: Hero[] = [];
+  
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
-  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
 }
